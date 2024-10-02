e1c483a3687492852da64e34b61bd41fadbb506d
Ibrohimjon-Kurbanov <kurbanovolddev@gmail.com>
2024-10-03 00:38:46 +0200

    4.6-dars-vazifa

---
+++ js/index.js
@@ -0,0 +1,199 @@
+// 1-MASALA
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// 2-MASALA
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i);
+// }
+
+// 3-MASALA
+// let num = +prompt("Sonni kiriting:");
+// for (let i = 1; i <= num ** 2; i++) {
+//   console.log(i);
+// }
+
+// 4-MASALA
+// for (let i = 0; i <= 10; i += 2) {
+//   console.log(i);
+// }
+
+// 5-MASALA
+// for (let i = 1; i <= 15; i += 2) {
+//   console.log(i);
+// }
+
+// 6-MASALA
+// for (let i = 1; i <= 5; i++) {
+//   let num = +prompt(`${i}${"-sonni kiriting:"}`);
+//   console.log(num);
+// }
+
+// 7-MASALA
+// for (let i = 15; i >= 5; i--) {
+//   console.log(i);
+// }
+
+// 8-MASALA
+// let num = +prompt("Sonni kiriting:");
+// for (let i = 1; i <= num; i++) {
+//   console.log(`${i} * ${num} = ${i * num}`);
+// }
+
+// 9-MASALA
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//   let num = +prompt(i + "-sonni kiriting");
+//   sum += num;
+// }
+// console.log(counter);
+
+// 10-MASALA
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 5 == 0) console.log(i);
+// }
+
+// 11-MASALA
+
+// for (let i = 100; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// 12-MASALA
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum = sum += i;
+// }
+// console.log(sum);
+
+// 13-MASALA
+
+// let num1 = +prompt("1-son");
+// let num2 = +prompt("2-son");
+
+// if (num1 < num2) {
+//   for (let i = num1 + 1; i < num2; i++) {
+//     console.log(i);
+//   }
+// } else {
+//   for (let i = num1 - 1; i > num2; i--) {
+//     console.log(i);
+//   }
+// }
+
+// 14-MASALA
+
+// let num = +prompt("Sonni kiritng:");
+
+// for (let i = 1; i <= num; i++) {
+//   if (num % i == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 15-MASALA
+
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i * i);
+// }
+
+// 16-MASALA
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 7 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 17-MASALA
+
+// for (let i = 1; i <= 10; i++) {
+//   for (let e = 1; e <= 10; e++) {
+//     console.log(`${i} * ${e} = ${i * e}`);
+//   }
+// }
+
+// 18-MASALA
+
+// let num = +prompt("Sonni kiriting:");
+// let sum = 0;
+// for (let i = 1; i <= num; i++) {
+//   if (num % i == 0) {
+//     console.log(i);
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+// 19-MASALA
+// let sum = 0;
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 == 1) {
+//     sum += i;
+//   }
+// }
+
+// console.log(sum);
+
+// 20-MASALA
+// let num1 = +prompt("1-son");
+// let num2 = +prompt("2-son");
+
+// for (let i = num1; i <= num2; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 21-MASALA
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 1) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+// 22-MASALA
+
+// let num = +prompt("Sonni kiritng:");
+
+// let factorial = 1;
+
+// for (let i = 1; i <= num; i++) {
+//   factorial *= i;
+// }
+
+// console.log(factorial);
+
+// 23-MASALA
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 === 0 || i % 5 === 0) {
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// 24-MASALA
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 4 == 0 && i % 6 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 25-MASALA
+// let num1 = +prompt("1-son");
+// let num2 = +prompt("2-son");
+// let sum = 0;
+
+// for (let i = num1; i <= num2; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//     sum += i;
+//   }
+// }
+// console.log(sum);
